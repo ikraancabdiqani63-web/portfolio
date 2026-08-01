a2bfeeb3bb21853cf60308053fb7319d794cc9cd
ikraancabdiqani63-web <ikraancabdiqani63@gmail.com>
2026-08-02 01:35:09 +0300

    Delete visa-app directory

--- visa-app/src/pages/Formpage.jsx
+++
@@ -1,9 +0,0 @@
-function FormPage() {
-  return (
-    <div className="page">
-      <h1>Visa Application Form</h1>
-    </div>
-  );
-}
-
-export default FormPage;
--- visa-app/src/pages/Reviewpage.jsx
+++
@@ -1,9 +0,0 @@
-function ReviewPage() {
-  return (
-    <div className="page">
-      <h1>Review Application</h1>
-    </div>
-  );
-}
-
-export default ReviewPage;
