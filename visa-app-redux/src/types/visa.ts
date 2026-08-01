b75fbcf47f010fd6b5123bd272ef69537a21e804
ikraancabdiqani63-web <ikraancabdiqani63@gmail.com>
2026-08-02 01:37:57 +0300

    Delete visa-app-redux directory

--- visa-app-redux/src/types/visa.ts
+++
@@ -1,8 +0,0 @@
-export interface VisaApplication {
-  id: number;
-  fullName: string;
-  passport: string;
-  country: string;
-  visaType: string;
-  status: "Pending" | "Approved" | "Rejected";
-}
